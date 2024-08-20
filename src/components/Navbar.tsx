34481bd8de75f4a2ce0b0c8cc9695e3d9326cb09
Erin Geier <erin.geier@gmail.com>
2024-08-20 15:42:54 -0500

    location in bold in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 import { signOut } from 'aws-amplify/auth';
 import { useState, useEffect } from 'react';
@@ -8,6 +8,11 @@ import { Hub } from 'aws-amplify/utils';
 
 const Navbar: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const location = useLocation();
+
+  // Determine the current path
+  const currentPath = location.pathname;
   //Always listen for a change in signin status
   Hub.listen('auth', ({ payload }) => {
     switch (payload.event) {
@@ -22,6 +27,8 @@ const Navbar: React.FC = () => {
 
     }
   });
+
+  
 
   //Set signin status upon first load of navbar
   useEffect(() => {
@@ -56,12 +63,12 @@ const Navbar: React.FC = () => {
     <div>
     <nav className="navbar">
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/download">Download</Link></li>
-        <li><Link to="/admin">Upload</Link></li>
-        <li><Link to="/guide">Guide</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/"  className={currentPath === '/' ? 'current-page' : ''} >Home</Link></li>
+        <li><Link to="/search"  className={currentPath === '/search' ? 'current-page' : ''}>Search</Link></li>
+        <li><Link to="/download"  className={currentPath === '/download' ? 'current-page' : ''}>Download</Link></li>
+        <li><Link to="/admin"  className={currentPath === '/admin' ? 'current-page' : ''}>Upload</Link></li>
+        <li><Link to="/guide"  className={currentPath === '/guide' ? 'current-page' : ''}>Guide</Link></li>
+        <li><Link to="/about"  className={currentPath === '/about' ? 'current-page' : ''}>About</Link></li>
         <li><a href="https://peabody.az1.qualtrics.com/jfe/preview/previewId/0419baa6-934b-4d65-a11c-e8e6f79c85b9/SV_eRQUVmfS4d7q4yq?Q_CHL=preview&Q_SurveyVersionID=current" target="_blank" rel="noopener noreferrer">
               Feedback
             </a></li>

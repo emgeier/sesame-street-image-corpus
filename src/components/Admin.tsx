f3db08882b48ef36e2501c263e056fab4867cc45
Erin Geier <erin.geier@gmail.com>
2024-08-21 09:25:07 -0500

    log in required for Admin access restored

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { uploadData, getUrl } from 'aws-amplify/storage';
+import { Authenticator } from '@aws-amplify/ui-react';
+import CustomHeader from './CustomMessaging';
 
 const Admin: React.FC = () => {
   //Individual image file upload
@@ -113,8 +115,13 @@ const Admin: React.FC = () => {
       setMessage(undefined); // Clear the message after 5 seconds
     }, 5000);
   };
+  const components = {
+    Header: CustomHeader,
+  };
 
   return (
+    <Authenticator hideSignUp className="authenticator-popup" components={components}>
+    {({  }) => (
     <div className="main-content">
       <h3>Upload Folder</h3>
       <input
@@ -144,6 +151,8 @@ const Admin: React.FC = () => {
       </button>
       {url && <img src={url} alt="Uploaded file" style={{ maxWidth: '100%', height: 'auto' }} />}
     </div>
+    )}
+</Authenticator>
   );
 };
 

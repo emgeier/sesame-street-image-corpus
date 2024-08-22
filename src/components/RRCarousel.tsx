24c85a188ae59167a54dbe4d822f5b06d54c90fa
Erin Geier <erin.geier@gmail.com>
2024-08-22 13:39:50 -0500

    images of matching sizes added

--- src/components/RRCarousel.tsx
+++ src/components/RRCarousel.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 
 import image1 from "../assets/images/00130.png";
-import image2 from "../assets/images/00293.png";
-import image3 from "../assets/images/00633.png";
-import image4 from "../assets/images/S48-E4821_00085.png";
+import image2 from "../assets/images/S48-E4821_01079.png";
+import image3 from "../assets/images/S48-E4821_00085.png";
+
 
 const RRCarousel: React.FC = () => (
   <div className="carousel-wrapper">
@@ -14,13 +14,10 @@ const RRCarousel: React.FC = () => (
         <img src={image1} alt="Slide 1" />
       </div>
       <div>
-        <img src={image2} alt="Slide 2" />
-      </div>
+        <img src={image2} alt="Slide 4" />
+      </div>    
       <div>
-        <img src={image3} alt="Slide 3" />
-      </div>
-      <div>
-        <img src={image4} alt="Slide 4" />
+        <img src={image3} alt="Slide 4" />
       </div>      
     </Carousel>
   </div>

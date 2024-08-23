3b21c78f9f527647fac30e24bd9d0d5b8f69ae82
Erin Geier <erin.geier@gmail.com>
2024-08-23 18:50:39 -0500

    footer width adjusted for mobile devices to match navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,8 +8,7 @@ function Home() {
     <div className="main-content">
       <main>
         <h1 className='intro'>Sesame Street Image Corpus</h1>
-        <div className = 'carousel'>
-        <RRCarousel ></RRCarousel></div>
+        <RRCarousel ></RRCarousel>
         <div className='separator'></div>
         <p className='indented'>
           This repository of images from Sesame Street enables machine learning on images that have had a profound impact on child development for over 50 years.

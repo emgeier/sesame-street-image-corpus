e530a14b778a6e5416471a8d886c3c908f5ba8a9
Erin Geier <erin.geier@gmail.com>
2024-08-23 16:58:48 -0500

    changed navbar and home carousel size for small screens

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,13 +8,14 @@ function Home() {
     <div className="main-content">
       <main>
         <h1 className='intro'>Sesame Street Image Corpus</h1>
-        <RRCarousel></RRCarousel>
+        <div className = 'carousel'>
+        <RRCarousel ></RRCarousel></div>
         <div className='separator'></div>
         <p className='indented'>
           This repository of images from Sesame Street enables machine learning on images that have had a profound impact on child development for over 50 years.
         </p>
         <img src={sesameStreetImage} alt="Sesame Street" className="sesame-image" />
-        {/* Home page content */}
+        
         <footer />
       </main>
     </div>

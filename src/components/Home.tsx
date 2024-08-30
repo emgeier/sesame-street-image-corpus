19cda4dfe1c6403750a50b9efafdc351f5d3243f
Erin Geier <erin.geier@gmail.com>
2024-08-30 08:40:25 -0500

    changed back to SSIC

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
         <div className='separator'></div>
         <main>
         <br/>
-        <p className='small indented'>The Sesame Street Image Archive (SSIA) is an open-access, searchable database of more than 35,000 unique images taken from over 4,000 original Sesame Street episodes. Using spatial bounding boxes to map different categories of visual data, each image is precisely annotated to identify target categories, such as faces, words, digits, and even castles and skylines. SSIC aims to innovate new forms of machine learning and AI research within and between disciplines, from neuroscience to theater and puppetry.</p>
+        <p className='small indented'>The Sesame Street Image Corpus (SSIC) is an open-access, searchable database of more than 35,000 unique images taken from over 4,000 original Sesame Street episodes. Using spatial bounding boxes to map different categories of visual data, each image is precisely annotated to identify target categories, such as faces, words, digits, and even castles and skylines. SSIC aims to innovate new forms of machine learning and AI research within and between disciplines, from neuroscience to theater and puppetry.</p>
         <div className='separator'></div>
         <p className='small indented'>Sesame Street was first broadcast in 1969 to bring educational content into children’s homes in the mornings before school. In the more than 50 years since, Sesame Street has demonstrated a lasting impact on early childhood education, improving literacy, numeracy, and social-emotional skills for generations of children worldwide.</p>
         <div className='separator'></div>
